78a863ae8f9fa3744e79642e6e5413e108e43c07
hg2025 <harshagundavelli@gmail.com>
2025-10-29 16:55:35 -0500

    harsha new made changes

--- Hangman.java
+++ Hangman.java
@@ -10,29 +10,35 @@ public class Hangman{
         System.out.println("Enter guess word: ");
         String word = Input.nextLine();
         boolean wordGuessed = false;
-        int correctGuessesCount = 0;
+        int correctGuessesCount = word.length();
+        boolean correctLetter = false;
 
         int lives = 6;
-        while((lives != 0) || (wordGuessed = false)) {
+        while((lives != 0) && (wordGuessed = false)) {
             System.out.println("Word length is: " + word.length());
             System.out.println("Enter your guess: ");
             char guess = Input.next().charAt(0);
             for (int i = 0; i < word.length(); i++) {
                 if (word.charAt(i) == guess) {
+                    correctLetter = true;
                     System.out.println("Guess correct!");
-                    correctGuessesCount = word.length()-1;
+                    correctGuessesCount--;
                     if(correctGuessesCount == 0){
                         System.out.println("YOU GUESSED THE WORD!");
                         wordGuessed = true;
+
                     }
                     break;
-                } else {
-                    System.out.println("Guess wrong!");
-                    lives--;
-                    System.out.println("Lives left:" + lives);
-                    break;
                 }
+
             }
+            if (wordGuessed) IO.println("wordguess is true");
+            if (!correctLetter){
+                System.out.println("Guess wrong!");
+                lives--;
+                System.out.println("Lives left:" + lives);
+            }
+
         }
        
     }
