6b23aea0abcf0e682f74c29183424a961fa3ea02
“Osi” <oobasuyi@uncc.edu>
2025-10-29 17:01:09 -0500

    commit to get changes

--- Hangman.java
+++ Hangman.java
@@ -10,35 +10,29 @@ public class Hangman{
         System.out.println("Enter guess word: ");
         String word = Input.nextLine();
         boolean wordGuessed = false;
-        int correctGuessesCount = word.length();
-        boolean correctLetter = false;
+        int correctGuessesCount = 0;
 
         int lives = 6;
-        while((lives != 0) && (wordGuessed = false)) {
+        while((lives != 0) || (wordGuessed = false)) {
             System.out.println("Word length is: " + word.length());
             System.out.println("Enter your guess: ");
             char guess = Input.next().charAt(0);
             for (int i = 0; i < word.length(); i++) {
                 if (word.charAt(i) == guess) {
-                    correctLetter = true;
                     System.out.println("Guess correct!");
-                    correctGuessesCount--;
+                    correctGuessesCount = word.length()-1;
                     if(correctGuessesCount == 0){
                         System.out.println("YOU GUESSED THE WORD!");
                         wordGuessed = true;
-
                     }
                     break;
+                } else {
+                    System.out.println("Guess wrong!");
+                    lives--;
+                    System.out.println("Lives left:" + lives);
+                    break;
                 }
-
             }
-            if (wordGuessed) IO.println("wordguess is true");
-            if (!correctLetter){
-                System.out.println("Guess wrong!");
-                lives--;
-                System.out.println("Lives left:" + lives);
-            }
-
         }
        
     }
